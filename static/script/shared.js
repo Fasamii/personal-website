7c0f83bad5dbe3736627597e3b23ff434d9dea0d
Fasamii <fasami0512@gmail.com>
2025-07-27 01:20:24 +0200

    glitch effect & favicon

---
+++ static/script/shared.js
@@ -0,0 +1,44 @@
+document.querySelectorAll('.glitch').forEach(item => {
+	const originalText = item.textContent;
+	let glitchInterval = null;
+	let isGlitching = false;
+
+	item.addEventListener('mouseenter', function() {
+		if (isGlitching) return;
+
+		isGlitching = true;
+		let glitchCount = 0;
+
+		glitchInterval = setInterval(() => {
+			const glitchChars = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+			let glitched = '';
+
+			for (let i = 0; i < originalText.length; i++) {
+				if (Math.random() < 0.3) {
+					glitched += glitchChars[Math.floor(Math.random() * glitchChars.length)];
+				} else {
+					glitched += originalText[i];
+				}
+			}
+
+			this.textContent = glitched;
+			glitchCount++;
+
+			if (glitchCount > Math.min(originalText.length, 8)) {
+				this.textContent = originalText;
+				clearInterval(glitchInterval);
+				glitchInterval = null;
+				isGlitching = false;
+			}
+		}, 50);
+	});
+
+	item.addEventListener('mouseleave', function() {
+		if (glitchInterval) {
+			clearInterval(glitchInterval);
+			glitchInterval = null;
+		}
+		isGlitching = false;
+		this.textContent = originalText;
+	});
+});
