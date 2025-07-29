0b5a47820041921a885fd1d952d0485e42a661fa
Fasamii <fasami0512@gmail.com>
2025-07-29 22:43:59 +0200

    automated age

--- static/script/shared.js
+++ static/script/shared.js
@@ -42,3 +42,48 @@ document.querySelectorAll('.glitch').forEach(item => {
 		this.textContent = originalText;
 	});
 });
+
+const my_age = new Date(new Date() - new Date(2008, 12, 5)).getFullYear() - 1970;
+document.querySelectorAll(`#age`).forEach(item => {
+	item.textContent = my_age > 0 ? my_age : "not born yet wtf";
+});
+
+// const languageColors = {
+// 	'JavaScript': '#f1e05a',
+// 	'TypeScript': '#2b7489',
+// 	'Python': '#3572A5',
+// 	'Java': '#b07219',
+// 	'C++': '#f34b7d',
+// 	'C': '#555555',
+// 	'C#': '#239120',
+// 	'PHP': '#4F5D95',
+// 	'Ruby': '#701516',
+// 	'Go': '#00ADD8',
+// 	'Rust': '#dea584',
+// 	'Swift': '#ffac45',
+// 	'Kotlin': '#F18E33',
+// 	'Dart': '#00B4AB',
+// 	'HTML': '#e34c26',
+// 	'CSS': '#1572B6',
+// 	'SCSS': '#c6538c',
+// 	'Vue': '#2c3e50',
+// 	'React': '#61DAFB',
+// 	'Shell': '#89e051',
+// 	'PowerShell': '#012456',
+// 	'Dockerfile': '#384d54',
+// 	'YAML': '#cb171e',
+// 	'JSON': '#292929',
+// 	'Markdown': '#083fa1',
+// 	'Lua': '#093fa1',
+// };
+//
+// document.querySelectorAll(`#langsp`).forEach(item => {
+// 	["C", "Lua", "Rust"].forEach(element => {
+// 		const langdiv = document.createElement(`div`);
+// 		langdiv.style.backgroundColor = languageColors[element];
+// 		langdiv.style.width = "33%"
+// 		langdiv.appendChild(document.createTextNode(element.toUpperCase()));
+// 		item.appendChild(langdiv);
+// 	});
+// 	console.log(item);
+// });
